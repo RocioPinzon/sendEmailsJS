6d008dbd2a0129362613b3df0c3a06708e35331f
Rocio Pinzon <pinzonbayonr@gmail.com>
2024-01-10 11:12:59 +0100

    Update Send Email

--- js/app.js
+++ js/app.js
@@ -1,34 +1,85 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    //
+
+    const dataEmail = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
     // Seleccionar elementos
     const inputEmail =  document.querySelector('#email');
     const inputAsunto =  document.querySelector('#asunto');
     const inputMensaje =  document.querySelector('#mensaje');
-    //const inputFormulario =  document.querySelector('#formulario');
+    const formulario =  document.querySelector('#formulario');
+    const btnSubmit =  document.querySelector('#formulario button[type="submit"]');
+    const btnReset =  document.querySelector('#formulario button[type="reset"]');
+    const spinner =  document.querySelector('#spinner');
 
     // Llamar addEventListener
-    inputEmail.addEventListener('blur',validar);
+    inputEmail.addEventListener('input',validar);
     inputAsunto.addEventListener('blur',validar);
     inputMensaje.addEventListener('blur',validar);
+    formulario.addEventListener('submit', enviarEmail);
+    btnReset.addEventListener('click', function(e) {
+        e.preventDefault();
+        resetForm();
+    });
+
+    function resetForm(){
+        // Reiniciar objeto
+        dataEmail.email= '';
+        dataEmail.asunto= '';
+        dataEmail.mensaje= '';
+
+        formulario.reset();
+        comprobarEmail();
+    }
+    
+    function enviarEmail(e){
+        e.preventDefault();
+        console.log('enviando...');
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+            resetForm();
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent='Mensaje enviado correctamente';
+            formulario.appendChild(alertaExito);
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 4000);
+        }, 3000);
+    }
 
     function validar(event){
         
-        validarVacio(event);
-
-    }
-
-    function validarVacio(event){
-        
         if(event.target.value.trim() === ''){
             mostrarAlerta(`El campo ${event.target.id} es obligatorio`, event.target.parentElement);
+            dataEmail[event.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        if(event.target.id === 'email' && !validarEmail(event.target.value)){
+            mostrarAlerta(`El campo ${event.target.id} debe tener un formato de mail`, event.target.parentElement);
+            dataEmail[event.target.name] = '';
+            comprobarEmail();
             return;
         }
         
         limpiarAlerta(event.target.parentElement);
-        console.log("CORRECTO");
-        const valueInputEmail = event.target.value.trim();
-        console.log("valueInputEmail - " + valueInputEmail);
-        
+
+        // Asignar valores
+        dataEmail[event.target.name] = event.target.value.trim().toLowerCase();
+        //console.log(dataEmail);
+        console.log("----");
+
+        // Comprobar email
+        comprobarEmail();
     }
 
     function mostrarAlerta(mensaje, ref){
@@ -51,5 +102,24 @@ document.addEventListener('DOMContentLoaded', function(){
         if (parrafoAlert) {
             parrafoAlert.remove();
         }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+        //console.log(resultado);
+
+        return resultado;
+    }
+    function comprobarEmail(){
+        console.log(dataEmail);
+        if(Object.values(dataEmail).includes('')) {
+            btnSubmit.classList.add('opacity-50')
+            btnSubmit.disabled = true;
+            return;
+        }
+        
+        btnSubmit.classList.remove('opacity-50')
+        btnSubmit.disabled = false;
     }
 });
